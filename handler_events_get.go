ad148a1e66c4a5b31709f0cf3496c7b92d891c20
David <davidbosnic99@gmail.com>
2025-06-23 12:53:15 -0600

    changes response format

--- handler_events_get.go
+++ handler_events_get.go
@@ -10,6 +10,10 @@ import (
 )
 
 func (config *apiConfig) handlerEventsGet(w http.ResponseWriter, r *http.Request) {
+	type event struct {
+		Title     string `json:"title"`
+		StartWhen string `json:"startWhen"`
+	}
 	url := fmt.Sprintf("https://www.googleapis.com/calendar/v3/calendars/%s/events", config.calendarID)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -44,10 +48,21 @@ func (config *apiConfig) handlerEventsGet(w http.ResponseWriter, r *http.Request
 		log.Printf("GET /calendar/events Error unmarshaling body %v\n", err)
 		return
 	}
+	var events []event
 	for _, item := range calendarEvent.Items {
-		log.Printf("Event: %s @ %s", item.Summary, item.Start.DateTime)
+		events = append(events, event{
+			Title:     item.Summary,
+			StartWhen: item.Start.DateTime,
+		})
 	}
+	dat, err := json.Marshal(events)
+	if err != nil {
+		log.Printf("Error marshaling chirps JSON: %s", err)
+		w.Write(dat)
+		return
+	}
+
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte(body))
+	w.Write(dat)
 }
