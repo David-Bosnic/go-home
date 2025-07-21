66a5ccbdec5b50edc1be8944b4a986abd145051d
David <davidbosnic99@gmail.com>
2025-07-21 14:44:32 -0600

    added template env

--- backend/main.go
+++ backend/main.go
@@ -105,7 +105,8 @@ type apiConfig struct {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		localHost := "http://localhost:" + os.Getenv("PORT")
+		w.Header().Set("Access-Control-Allow-Origin", localHost)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
